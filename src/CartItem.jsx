5a6b690d30159f754ba76b6cd957e716a6ed98f3
Rekin226 <abdoulrachid03@gmail.com>
2024-09-13 03:43:06 -0400

    add message when click on checkout

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -63,7 +63,6 @@ const CartItem = ({ onContinueShopping }) => {
 
   return (
     <div className="cart-container">
-
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
       <div>
         {cart.map(item => (
@@ -87,7 +86,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
